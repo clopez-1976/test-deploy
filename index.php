ada2e11aa04b258510f46c183f0a309a8032365c
clopez-1976 <102628050+clopez-1976@users.noreply.github.com>
2022-12-17 09:06:26 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Generated with CS2PHP
+*/
+namespace Lang\Php\Examples\HelloWorld {
+    class Index
+    {
+        public static function PhpMain()
+        {
+            echo 'Hello world!';
+        }
+    }
+}
+namespace {
+    \Lang\Php\Examples\HelloWorld\Index::PhpMain();
+}
